af8b9bfc03ec8ce314f7f683a86ae958ce43c7ee
Dhaval Khimani <103636161+dhavalkgit@users.noreply.github.com>
2023-11-01 09:51:02 +0530

    Update CP_Template

--- CP_Template.cpp
+++ CP_Template.cpp
@@ -5,9 +5,16 @@ using namespace std;
 #define ll long long int
 #define vi vector<int>
 #define vll vector<long long int>
-#define vp push_back
+#define mii map<int,int>
+#define mci map<char,int>
+#define mil map<int,long long int>
+#define p pair<int,int>
+#define pb push_back;
+#define ppb pop_back;
+
 #define lp(i,s,e) for(int i=s;i<e;i++)
 #define te(t) while(t--)
+
 int main() {
   
   return 0;
