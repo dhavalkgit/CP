aba55d5cc1b7c4a4d84d15a661eb5512715ea98b
Dhaval Khimani <103636161+dhavalkgit@users.noreply.github.com>
2024-02-29 15:25:10 +0530

    Cycle detection in directed graph using DFS

--- Algo/detectCycleDFS.cpp
+++ Algo/detectCycleDFS.cpp
@@ -1,15 +1,15 @@
 bool iscycle(vector<vector<int>>&adj, vector<bool>&visi, vector<bool>&dfs,int node){
     visi[node]=true;
     dfs[node]=true;
-    bool ans = true;
 
     for(auto el:adj[node]){
         if(!visi[el]){
-            ans |= iscycle(adj,visi,dfs,el);
+            bool ans = iscycle(adj,visi,dfs,el);
+            if(ans) return true;
         }
-        else if(dfs[el]) return false;
+        else if(dfs[el]) return true;
     }
     
     dfs[node]=false;
-    return ans;
+    return false;
 }
